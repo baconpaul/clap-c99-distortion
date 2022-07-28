09f714fd26332cc45ead4c64ad035cc10265f3f7
Paul Walker <paul@pwjw.com>
2022-07-28 09:20:18 -0400

    State; some cleanups
    1. State is implemented
    2: The DSP doesn't just fall through
    3. Replace SOFT with a wavefolder

--- src/clap-c99-distortion.c
+++ src/clap-c99-distortion.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 
 #include <clap/clap.h>
+#include <math.h>
+#include <assert.h>
 
 static const clap_plugin_descriptor_t s_c99dist_desc = {
     .clap_version = CLAP_VERSION_INIT,
@@ -27,7 +29,7 @@ enum ClipType
 {
     HARD = 0,
     SOFT = 1,
-    ASYM = 2
+    FOLD = 2
 };
 
 enum ParamIds
@@ -44,12 +46,14 @@ typedef struct
     const clap_host_latency_t *hostLatency;
     const clap_host_log_t *hostLog;
     const clap_host_thread_check_t *hostThreadCheck;
-    const clap_host_params_t  *hostParams;
+    const clap_host_params_t *hostParams;
 
     float drive;
     float mix;
-    int mode;
+    int32_t mode;
 } clap_c99_distortion_plug;
+
+static void c99dist_process_event(clap_c99_distortion_plug *plug, const clap_event_header_t *hdr);
 
 /////////////////////////////
 // clap_plugin_audio_ports //
@@ -79,29 +83,25 @@ static const clap_plugin_audio_ports_t s_c99dist_audio_ports = {
     .get = c99dist_audio_ports_get,
 };
 
-
 //////////////////
 // clap_latency //
 //////////////////
 
-uint32_t c99dist_latency_get(const clap_plugin_t *plugin)
-{
-    return 0;
-}
+uint32_t c99dist_latency_get(const clap_plugin_t *plugin) { return 0; }
 
 static const clap_plugin_latency_t s_c99dist_latency = {
     .get = c99dist_latency_get,
 };
-
 
 //////////////////
 // clap_porams //
 //////////////////
 
 uint32_t c99dist_param_count(const clap_plugin_t *plugin) { return 3; }
-bool c99dist_param_get_info(const clap_plugin_t *plugin, uint32_t param_index, clap_param_info_t *param_info)
+bool c99dist_param_get_info(const clap_plugin_t *plugin, uint32_t param_index,
+                            clap_param_info_t *param_info)
 {
-    switch(param_index)
+    switch (param_index)
     {
     case 0: // drive
         param_info->id = pid_DRIVE;
@@ -109,7 +109,7 @@ bool c99dist_param_get_info(const clap_plugin_t *plugin, uint32_t param_index, c
         param_info->module[0] = 0;
         param_info->default_value = 0.;
         param_info->min_value = -1;
-        param_info->max_value = 3;
+        param_info->max_value = 6;
         param_info->flags = CLAP_PARAM_IS_AUTOMATABLE;
         param_info->cookie = NULL;
         break;
@@ -142,7 +142,7 @@ bool c99dist_param_get_value(const clap_plugin_t *plugin, clap_id param_id, doub
 {
     clap_c99_distortion_plug *plug = plugin->plugin_data;
 
-    switch(param_id)
+    switch (param_id)
     {
     case pid_DRIVE:
         *value = plug->drive;
@@ -162,11 +162,11 @@ bool c99dist_param_get_value(const clap_plugin_t *plugin, clap_id param_id, doub
 
     return false;
 }
-bool c99dist_param_value_to_text(
-    const clap_plugin_t *plugin, clap_id param_id, double value, char *display, uint32_t size)
+bool c99dist_param_value_to_text(const clap_plugin_t *plugin, clap_id param_id, double value,
+                                 char *display, uint32_t size)
 {
     clap_c99_distortion_plug *plug = plugin->plugin_data;
-    switch(param_id)
+    switch (param_id)
     {
     case pid_DRIVE:
     case pid_MIX:
@@ -174,36 +174,113 @@ bool c99dist_param_value_to_text(
         return true;
         break;
     case pid_MODE:
-        snprintf(display, size, "%d", (int)value);
+    {
+        int v = (int)value;
+        switch (v)
+        {
+        case 0:
+            snprintf(display, size, "Hard Clip");
+            break;
+        case 1:
+            snprintf(display, size, "Soft Clip (Tanh)");
+            break;
+        case 2:
+            snprintf(display, size, "Simple Folder");
+            break;
+        }
+
         return true;
-        break;
+    }
+    break;
     }
     return false;
 }
-bool c99dist_text_to_value(const clap_plugin_t *plugin,
-                      clap_id              param_id,
-                      const char          *display,
-                      double              *value)
+bool c99dist_text_to_value(const clap_plugin_t *plugin, clap_id param_id, const char *display,
+                           double *value)
 {
     // I'm not going to bother to support this
     return false;
 }
-void c99dist_flush(const clap_plugin_t        *plugin,
-              const clap_input_events_t  *in,
-              const clap_output_events_t *out)
+void c99dist_flush(const clap_plugin_t *plugin, const clap_input_events_t *in,
+                   const clap_output_events_t *out)
 {
+    clap_c99_distortion_plug *plug = plugin->plugin_data;
+    int s = in->size(in);
+    int q;
+    for (q = 0; q < s; ++q)
+    {
+        const clap_event_header_t *hdr = in->get(in, q);
 
+        c99dist_process_event(plug, hdr);
+    }
 }
 
-static const clap_plugin_params_t s_c99dist_params = {
-    .count = c99dist_param_count,
-    .get_info = c99dist_param_get_info,
-    .get_value = c99dist_param_get_value,
-    .value_to_text = c99dist_param_value_to_text,
-    .text_to_value = c99dist_text_to_value,
-    .flush = c99dist_flush
-};
+static const clap_plugin_params_t s_c99dist_params = {.count = c99dist_param_count,
+                                                      .get_info = c99dist_param_get_info,
+                                                      .get_value = c99dist_param_get_value,
+                                                      .value_to_text = c99dist_param_value_to_text,
+                                                      .text_to_value = c99dist_text_to_value,
+                                                      .flush = c99dist_flush};
 
+bool c99dist_state_save(const clap_plugin_t *plugin, const clap_ostream_t *stream)
+{
+    clap_c99_distortion_plug *plug = plugin->plugin_data;
+
+    // We need to save 2 doubles and an int to save our state plus a version. This is, of course, a
+    // terrible implementation of state. You should do better.
+    assert(sizeof(float) == 4);
+    assert(sizeof(int32_t) == 4);
+
+    int buffersize = 16;
+    char buffer[16];
+
+    int32_t version = 1;
+    memcpy(buffer, &version, sizeof(int32_t));
+    memcpy(buffer + 4, &(plug->drive), sizeof(float));
+    memcpy(buffer + 8, &(plug->mix), sizeof(float));
+    memcpy(buffer + 12, &(plug->mode), sizeof(int32_t));
+
+    int written = 0;
+    char *curr = buffer;
+    while (written != buffersize)
+    {
+        int thiswrite = stream->write(stream, curr, buffersize - written);
+        if (thiswrite < 0)
+            return false;
+        curr += thiswrite;
+        written += thiswrite;
+    }
+
+    return true;
+}
+
+bool c99dist_state_load(const clap_plugin_t *plugin, const clap_istream_t *stream) {
+    clap_c99_distortion_plug *plug = plugin->plugin_data;
+
+    int buffersize = 16;
+    char buffer[16];
+
+    int read = 0;
+    char *curr = buffer;
+    while( read != buffersize)
+    {
+        int thisread = stream->read(stream, curr, buffersize - read);
+        if (thisread < 0)
+            return false;
+        curr += thisread;
+        read += thisread;
+    }
+
+    int32_t version;
+    memcpy(&version, buffer, sizeof(int32_t));
+    memcpy(&plug->drive, buffer + 4, sizeof(float));
+    memcpy(&plug->mix, buffer + 8, sizeof(float));
+    memcpy(&plug->mode, buffer + 12, sizeof(int32_t));
+
+    return true;
+}
+static const clap_plugin_state_t s_c99dist_state = {.save = c99dist_state_save,
+                                                    .load = c99dist_state_load};
 
 /////////////////
 // clap_plugin //
@@ -224,7 +301,11 @@ static bool c99dist_init(const struct clap_plugin *plugin)
     return true;
 }
 
-static void c99dist_destroy(const struct clap_plugin *plugin) {}
+static void c99dist_destroy(const struct clap_plugin *plugin)
+{
+    clap_c99_distortion_plug *plug = plugin->plugin_data;
+    free(plug);
+}
 
 static bool c99dist_activate(const struct clap_plugin *plugin, double sample_rate,
                              uint32_t min_frames_count, uint32_t max_frames_count)
@@ -250,7 +331,7 @@ static void c99dist_process_event(clap_c99_distortion_plug *plug, const clap_eve
         {
             const clap_event_param_value_t *ev = (const clap_event_param_value_t *)hdr;
             // TODO: handle parameter change
-            switch( ev->param_id)
+            switch (ev->param_id)
             {
             case pid_DRIVE:
                 plug->drive = ev->value;
@@ -264,7 +345,6 @@ static void c99dist_process_event(clap_c99_distortion_plug *plug, const clap_eve
             }
             break;
         }
-
         }
     }
 }
@@ -315,38 +395,35 @@ static clap_process_status c99dist_process(const struct clap_plugin *plugin,
             float tl = in_l * (1.0 + plug->drive);
             float tr = in_r * (1.0 + plug->drive);
 
-
             // Obviously this is inefficient but
             switch (plug->mode)
             {
             case HARD:
             {
-                tl = ( tl > 1 ? 1 : tl < -1 ? -1 : tl);
-                tr = ( tr > 1 ? 1 : tr < -1 ? -1 : tr);
+                tl = (tl > 1 ? 1 : tl < -1 ? -1 : tl);
+                tr = (tr > 1 ? 1 : tr < -1 ? -1 : tr);
             }
+            break;
             case SOFT:
             {
-                tl = ( tl > 1 ? 1 : tl < -1 ? -1 : tl);
+                tl = (tl > 1 ? 1 : tl < -1 ? -1 : tl);
                 tl = 1.5 * tl - 0.5 * tl * tl * tl;
 
-                tr = ( tr > 1 ? 1 : tr < -1 ? -1 : tr);
+                tr = (tr > 1 ? 1 : tr < -1 ? -1 : tr);
                 tr = 1.5 * tr - 0.5 * tr * tr * tr;
             }
-            case ASYM:
+            break;
+            case FOLD:
             {
-                tl = ( tl > 1 ? 1 : tl < -1 ? -1 : tl);
-                if (tl < 0)
-                    tl = 1.5 * tl - 0.5 * tl * tl * tl;
-
-                tr = ( tr > 1 ? 1 : tr < -1 ? -1 : tr);
-                if (tr < 0)
-                    tr = 1.5 * tr - 0.5 * tr * tr * tr;
+                tl = sin(2.0 * M_PI * tl * tl * tl);
+                tr = sin(2.0 * M_PI * tr * tr * tr);
             }
+            break;
             }
 
             float mix = plug->mix;
-            out_l = mix * tl + (1.0-mix)* in_l;
-            out_r = mix * tr + (1.0-mix)* in_r;
+            out_l = mix * tl + (1.0 - mix) * in_l;
+            out_r = mix * tr + (1.0 - mix) * in_r;
 
             // store output samples
             process->audio_outputs[0].data32[0][i] = out_l;
@@ -365,6 +442,8 @@ static const void *c99dist_get_extension(const struct clap_plugin *plugin, const
         return &s_c99dist_audio_ports;
     if (!strcmp(id, CLAP_EXT_PARAMS))
         return &s_c99dist_params;
+    if (!strcmp(id, CLAP_EXT_STATE))
+        return &s_c99dist_state;
     return NULL;
 }
 
